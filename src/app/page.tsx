5798fd3f6c6f13d891dc1a6b121669f3ce5cf748
Nchouwat kpoumie <nchouwatkpoumi7@gmail.com>
2023-08-21 01:19:54 +0100

    Fetching datas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,29 @@
 import PaginationComponent from '@/components/paginationComponent'
 import SearchInputComponent from '@/components/searchInputComponent'
 import DropdownComponent from '@/components/selectComponent'
-import TableComponent from '@/components/tableComponent'
-import { Metadata } from 'next'
+import ProductComponent from '@/components/tableComponent'
+import { GetServerSideProps, Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'HOME | ADAA NextJS Test App',
   description: 'Make by NK$_R@0ul',
 }
 
-export default function Home() {
+async function fetchDatas() {
+  let a = await fetch("https://dummyjson.com/products?limit=10&skip=5").then(res => res.json())
+
+  return a;
+}
+
+
+export default async function Home() {
+  const datas = await fetchDatas();
+
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start border-black">
-      <div className="container flex flex-col items-center border-black mt-4">
+      <div className="container flex flex-col items-center border-black my-4">
         {/*  */}
         <div className="flex items-center justify-between w-3/4">
           <div className='border-black w-80'>
@@ -24,15 +35,14 @@ export default function Home() {
           </div>
         </div>
         {/*  */}
-        <div>
-          the table place !!!
+        <div className='my-8 w-3/4 border-2 border-black'>
+          <ProductComponent products={datas.products} />
         </div>
         {/*  */}
         <div className='w-3/4'>
           <div className='flex'>
             <PaginationComponent />
           </div>
-          <div></div>
         </div>
       </div>
     </main>
--- src/components/tableComponent.tsx
+++ src/components/tableComponent.tsx
@@ -1,103 +1,60 @@
 'use client'
-import { Alert, Table } from "flowbite-react"
+import { Avatar, Table } from "flowbite-react"
+import Image from "next/image";
+import { HiStar } from 'react-icons/hi';
 
-export default function TableComponent({ children }: any) {
+export default function ProductComponent(props: any) {
     return <>
-        <Table className="border-4 w-full border-black ">
-            <Table.Head className="w-full">
+        <Table hoverable className="w-full">
+            <Table.Head>
                 <Table.HeadCell>
-                    Product name
+                    ID
                 </Table.HeadCell>
                 <Table.HeadCell>
-                    Color
+                    Thumbnails
                 </Table.HeadCell>
                 <Table.HeadCell>
-                    Category
+                    Title
                 </Table.HeadCell>
                 <Table.HeadCell>
-                    Price
+                    Description
                 </Table.HeadCell>
                 <Table.HeadCell>
-                    <span className="sr-only">
-                        Edit
-                    </span>
+                    Categorie
+                </Table.HeadCell>
+                <Table.HeadCell>
+                    Prix
+                </Table.HeadCell>
+                <Table.HeadCell>
+                    La Marque
                 </Table.HeadCell>
             </Table.Head>
             <Table.Body className="divide-y">
-                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                        Apple MacBook Pro 17 &quot;
-                    </Table.Cell>
-                    <Table.Cell>
-                        Sliver
-                    </Table.Cell>
-                    <Table.Cell>
-                        Laptop
-                    </Table.Cell>
-                    <Table.Cell>
-                        $2999
-                    </Table.Cell>
-                    <Table.Cell>
-                        <a
-                            className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                            href="/tables"
-                        >
-                            <p>
-                                Edit
-                            </p>
-                        </a>
-                    </Table.Cell>
-                </Table.Row>
-                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                        <p>
-                            Microsoft Surface Pro
-                        </p>
-                    </Table.Cell>
-                    <Table.Cell>
-                        White
-                    </Table.Cell>
-                    <Table.Cell>
-                        Laptop PC
-                    </Table.Cell>
-                    <Table.Cell>
-                        $1999
-                    </Table.Cell>
-                    <Table.Cell>
-                        <a
-                            className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                            href="/tables"
-                        >
-                            <p>
-                                Edit
-                            </p>
-                        </a>
-                    </Table.Cell>
-                </Table.Row>
-                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                        Magic Mouse 2
-                    </Table.Cell>
-                    <Table.Cell>
-                        Black
-                    </Table.Cell>
-                    <Table.Cell>
-                        Accessories
-                    </Table.Cell>
-                    <Table.Cell>
-                        $99
-                    </Table.Cell>
-                    <Table.Cell>
-                        <a
-                            className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                            href="/tables"
-                        >
-                            <p>
-                                Edit
-                            </p>
-                        </a>
-                    </Table.Cell>
-                </Table.Row>
+                {props.products?.map((ele: any) => (
+                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={ele.id}>
+                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                            {ele.id}
+                        </Table.Cell>
+                        <Table.Cell className="flex items-center justify-center">
+                            <Avatar alt={ele.title} img={ele.image} className="border-2 border-black h-10 w-10" />
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                            {ele.title}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {ele.description}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {ele.category}
+                        </Table.Cell>
+                        <Table.Cell>
+                            ${ele.price}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {ele.brand}
+                        </Table.Cell>
+                    </Table.Row>
+                ))}
             </Table.Body>
         </Table>
     </>
